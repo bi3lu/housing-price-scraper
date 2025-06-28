6b3936bc2d87c07a364b89ad317a32a1448ba3ad
Jakub Bielecki <kuba.bielecki@outlook.com>
2025-06-28 19:45:09 +0200

    add get_houses_bs_content function
    the function takes two parameters: the province and the city from which we want to download the content

--- src/housing_scrapper/housing_scarpper.py
+++ src/housing_scrapper/housing_scarpper.py
@@ -6,17 +6,24 @@ Author: Jakub Bielecki
 Created: 28-06-2025
 """
 
+
 import requests
 from bs4 import BeautifulSoup as bs
 
 
-headers = {
+HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     "Accept-Language": "pl-PL,pl;q=0.9",
 }
 
-page_url = "https://www.otodom.pl/" # can be constant
-page = requests.get(page_url, headers=headers)
-soup = bs(page.content)
 
-print(soup.prettify())
+def get_houses_bs_content(voivodeship, city):
+    root_url = f"https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/{voivodeship}/{city}/"
+    page = requests.get(root_url, headers=HEADERS)
+    soup = bs(page.content)
+    
+    return soup
+
+
+soup_obj = get_houses_bs_content('opolskie', 'opole')
+print(soup_obj.prettify())
