62c026edf65dbd5f9a6e50e753c8b1d86dd77375
Jakub Bielecki <kuba.bielecki@outlook.com>
2025-06-28 20:49:05 +0200

    add little bit of objective approach

--- src/housing_scrapper/housing_scarpper.py
+++ src/housing_scrapper/housing_scarpper.py
@@ -12,6 +12,28 @@ import requests
 import json
 
 
+class HouseItem:
+    def __init__(self, title, price, city, street, area, rooms, house_url):
+        self.title = title
+        self.price = price
+        self.city = city
+        self. street = street
+        self.area = area
+        self.rooms = rooms
+        self.house_url = house_url
+    
+
+    def print_info(self):
+        print(f"Title: {self.title}")
+        print(f"Price: {self.price} PLN")
+        print(f"City: {self.city}")
+        print(f"Street: {self.street}")
+        print(f"Area: {self.area}")
+        print(f"Rooms: {self.rooms}")
+        print(f"Link: {self.house_url}")
+        print('*' * 100 + '\n')
+
+
 def fetch_otodom_next_data_json(voivodeship: str, city: str, page_num: int = 1):
     url = f"https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/{voivodeship}/{city}?page={page_num}"
     headers = {
@@ -40,19 +62,16 @@ def extract_listing_items(next_data_json):
 def get_item_info(item):
     title = item.get('title', 'no title')
     price = item.get('totalPrice', {}).get('value', 'no price')
-    city = item.get('location', {}).get('address', {}).get('city', {}).get('name', 'no city name')
-    area = item.get('areaInSquareMeters', 'no square meters area')
+    city = item.get('location', {}).get('address', {}).get('city', {}).get('name', 'no city')
+    street = item.get('location', {}).get('address', {}).get('street', 'no street')
+    area = item.get('areaInSquareMeters', 'no area')
     rooms = item.get('roomsNumber', 'no rooms')
-    link = f'https://www.otodom.pl/pl/oferta/{item.get('slug', '')}'
+    house_url = f"https://www.otodom.pl/pl/oferta/{item.get('slug', '')}"
 
-    print(f"{title}")
-    print(f"Cena: {price} PLN")
-    print(f"Miasto: {city}")
-    print(f"Powierzchnia: {area} m²")
-    print(f"Pokoje: {rooms}")
-    print(f"Link: {link}")
+    return HouseItem(title, price, city, street, area, rooms, house_url)
 
 
 next_data_json = fetch_otodom_next_data_json("opolskie", "opole", page_num=1)
 house_items = extract_listing_items(next_data_json)
-get_item_info(house_items[0])
+item = get_item_info(house_items[0])
+item.print_info()
