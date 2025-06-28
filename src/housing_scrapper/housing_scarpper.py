e32a3851215badd0582a01d9ff48cea7c3a44dbe
Jakub Bielecki <kuba.bielecki@outlook.com>
2025-06-28 20:31:13 +0200

    add extract_listing_items function

--- src/housing_scrapper/housing_scarpper.py
+++ src/housing_scrapper/housing_scarpper.py
@@ -12,7 +12,7 @@ import requests
 import json
 
 
-def get_raw_next_data_json(voivodeship: str, city: str, page_num: int = 1):
+def fetch_otodom_next_data_json(voivodeship: str, city: str, page_num: int = 1):
     url = f"https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/{voivodeship}/{city}?page={page_num}"
     headers = {
         "User-Agent": "Mozilla/5.0"
@@ -26,9 +26,18 @@ def get_raw_next_data_json(voivodeship: str, city: str, page_num: int = 1):
         raise Exception("Not found in <script id='__NEXT_DATA__'> tag.")
 
     json_data = json.loads(script_tag.string)
-    
+
     return json_data
 
 
-data = get_raw_next_data_json("opolskie", "opole", page_num=1)
-print(json.dumps(data, indent=2, ensure_ascii=False))
+def extract_listing_items(next_data_json):
+    try:
+        return next_data_json['props']['pageProps']['data']['searchAds']['items']
+    except KeyError:
+        raise Exception("Not found any house items.")
+
+
+raw_data = fetch_otodom_next_data_json("opolskie", "opole", page_num=1)
+houses_data = extract_listing_items(raw_data)
+
+print(json.dumps(houses_data, indent=2, ensure_ascii=False))
