d218d47ccaa55571b8bc64112636f5e6da5a87d6
Jakub Bielecki <kuba.bielecki@outlook.com>
2025-07-20 09:53:15 +0200

    extract model training to separate file

---
+++ src/model/xgboost/xgboost.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Description:
+
+Author: Jakub Bielecki
+Created: 20-07-2025
+"""
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+
+
+PATH = '../csv_data/processed'
+CITY = 'opole'
+
+
+df = pd.read_cs(f'{PATH}/{CITY}.csv')
+
+X = df.drop('price', axis=1)
+y = df['price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = XGBRegressor(
+    n_estimators=100,
+    learning_rate=0.1,
+    max_depth=6,
+    random_state=42,
+    n_jobs=-1,
+    tree_method='hist',
+    enable_categorical=False
+)
+
+model.fit(X_train, y_train)
